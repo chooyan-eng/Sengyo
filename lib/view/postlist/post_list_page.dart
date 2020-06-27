b28bab47ba872dfc7cdae78c3fdee666b5f138f2
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-06-27 18:20:55 +0900

    search text box on post list page

--- lib/view/postlist/post_list_page.dart
+++ lib/view/postlist/post_list_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:sengyo/view/app_drawer.dart';
 import 'package:sengyo/view/submit/submit_fish_page.dart';
+import 'package:sengyo/view/widget/app_colors.dart';
 
 class PostListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Container(
+          height: 44,
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: AppColors.theme,
+                ),
+              ),
+              suffixIcon: InkWell(
+                onTap: () {},
+                child: Icon(Icons.search),
+              ),
+            ),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           return Navigator.of(context).push(MaterialPageRoute(builder: (context) => SubmitFishPage()));
