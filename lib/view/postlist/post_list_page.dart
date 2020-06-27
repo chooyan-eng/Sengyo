b53153a7d1e1b552c0d7c951423f69f945916154
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-06-28 01:10:14 +0900

    add sort button to post list page

--- lib/view/postlist/post_list_page.dart
+++ lib/view/postlist/post_list_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:sengyo/view/app_drawer.dart';
 import 'package:sengyo/view/postlist/widget/pickup_list.dart';
 import 'package:sengyo/view/postlist/widget/post_list_item.dart';
-import 'package:sengyo/view/submit/submit_fish_page.dart';
 import 'package:sengyo/view/submit/submit_fish_scene.dart';
 import 'package:sengyo/view/widget/app_colors.dart';
+import 'package:sengyo/view/widget/app_text_style.dart';
 
 class PostListPage extends StatelessWidget {
   @override
@@ -41,8 +41,42 @@ class PostListPage extends StatelessWidget {
       drawer: AppDrawer(),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             PickupList(),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      'みんなのお魚メモ',
+                      style: AppTextStyle.label,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.sort, color: AppColors.accent),
+                          const SizedBox(width: 4),
+                          Text(
+                            '新着',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.accent,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
             ListView.builder(
               shrinkWrap: true,
               physics: ClampingScrollPhysics(),
